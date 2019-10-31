892f32d835a28b2305e15258e6d53b7072ff5fb4
christophermms <christophermms.ctt@gmail.com>
2019-10-31 10:23:55 -0300

    Menu superior

---
+++ src/components/header/header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Apps from "@material-ui/icons/Apps";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
+import "../../assets/css/main.css";
+
+class header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <Grid
+          className="MenuSuperior"
+          container
+          justify="flex-end"
+          alignItems="center"
+        >
+          <div className="SuperiorMenuText">
+            <a className="Subtitulo" href="www.google.com">
+              Gmail
+            </a>
+            <a className="Subtitulo" href="www.google.com">
+              Imagens
+            </a>
+          </div>
+          <Apps className="AppsIco" />
+          <div className="SuperiorAvatar">
+            <Avatar
+              className="AvatarUser"
+              alt="Avatar"
+              sizes="big"
+              src={this.props.ImgAvatar}
+            />
+          </div>
+        </Grid>
+      </>
+    );
+  }
+}
+
+export default header;
